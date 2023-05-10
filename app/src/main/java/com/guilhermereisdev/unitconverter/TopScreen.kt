e45d83c1c8fff658e1d7215653a388e9ea02185c
Guilherme Reis <guilhermereis2009@hotmail.com>
2023-05-10 04:48:40 -0300

    Cria o ResultBlock e exibe na tela o valor das conversões feitas.

--- app/src/main/java/com/guilhermereisdev/unitconverter/TopScreen.kt
+++ app/src/main/java/com/guilhermereisdev/unitconverter/TopScreen.kt
@@ -5,11 +5,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import java.math.RoundingMode
+import java.text.DecimalFormat
 
 @Composable
 fun TopScreen(list: List<Conversion>) {
     val selectedConversion: MutableState<Conversion?> = remember { mutableStateOf(null) }
     val inputText: MutableState<String> = remember { mutableStateOf("") }
+    val typedValue: MutableState<String> = remember { mutableStateOf("0.0") }
 
     ConversionMenu(list = list) {
         selectedConversion.value = it
@@ -17,7 +20,19 @@ fun TopScreen(list: List<Conversion>) {
 
     selectedConversion.value?.let {
         InputBlock(conversion = it, inputText = inputText) { input ->
-            Log.i("MYTAG", "User typed: $input")
+            typedValue.value = input
+        }
+    }
+
+    if (typedValue.value != "0.0") {
+        val input = typedValue.value.toDouble()
+        selectedConversion.value?.let {
+            val df = DecimalFormat("#.##")
+            df.roundingMode = RoundingMode.HALF_UP
+            val roundedResult = df.format(input * it.multiplyBy)
+            val message1 = "${typedValue.value} ${it.convertFrom} is equal to"
+            val message2 = "$roundedResult ${it.convertTo}"
+            ResultBlock(message1 = message1, message2 = message2)
         }
     }
 }
